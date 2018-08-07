d7e318c716246ac5b3a862d7c03d9f9d62017579
Richman Larry Clifford <larriche99@gmail.com>
2018-08-07 18:00:00 +0000

    Added model for books

--- catalog/models.py
+++ catalog/models.py
@@ -21,3 +21,22 @@ class Language(models.Model):
         """
         return self.name
 
+class Book(models.Model):
+    """Model representing a book (but not a specfic copy of a book). """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13,
+        help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book');
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        """String representing the Model object"""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('book-detail', args=[str(self.id)])
+
+
